6ecbabe1824e6cb617b785bff4d6d36e60aafdae
Bhagya-laxmi <50071928+Bhagya-laxmi@users.noreply.github.com>
2019-10-30 18:07:11 +0100

    Create onload.js

---
+++ onload.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<script type="text/javascript">
+function message()
+{
+    document.write("Hello, How are you?");
+alert("This alert box was called with the onload event");
+}
+</script>
+</head>
+<body onload="message()">
+</body>
+</html>
