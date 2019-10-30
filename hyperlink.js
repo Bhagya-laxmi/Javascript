4327db4d0bd5b701b4105084d17d8601e0e5e014
Bhagya-laxmi <50071928+Bhagya-laxmi@users.noreply.github.com>
2019-10-30 17:39:07 +0100

    Create hyperlink.js

---
+++ hyperlink.js
@@ -0,0 +1,13 @@
+<html>
+   <head>      
+      <script type = "text/javascript">
+         <!--
+         //-->
+      </script>   
+   </head>
+   
+   <body>   
+      <p>Click the following, This won't react at all...</p>
+      <a href = "https://www.wikipedia.org/">Click me!</a>     
+   </body>
+</html>
