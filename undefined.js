24eebd0e564aa62651fbaeed4acf6fee682fcb95
Bhagya-laxmi <50071928+Bhagya-laxmi@users.noreply.github.com>
2019-10-30 17:43:21 +0100

    Create undefined.js

---
+++ undefined.js
@@ -0,0 +1,21 @@
+<html>
+   <head>      
+      <script type = "text/javascript">
+         <!--
+            function getValue() {
+               var a;
+               
+               a = void (0);
+               document.write('a = ' + a  );
+            }
+         //-->
+      </script>      
+   </head>
+   
+   <body>
+      <p>Click the following to see the result:</p>
+      <form>
+         <input type = "button" value = "Click Me" onclick = "getValue();" />
+      </form>     
+   </body>
+</html>
