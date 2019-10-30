ae8db6e447f2584a53702f9aca0682b50da4da7d
Bhagya-laxmi <50071928+Bhagya-laxmi@users.noreply.github.com>
2019-10-30 16:56:50 +0100

    Create Prompt_box.js

---
+++ Prompt_box.js
@@ -0,0 +1,19 @@
+<html>
+   <head>     
+      <script type = "text/javascript">
+         <!--
+            function getValue() {
+               var retVal = prompt("Are you learning : ", "yes or no");
+               document.write("You have entered : " + retVal);
+            }
+         //-->
+      </script>      
+   </head>
+   
+   <body>
+      <p>Click the following button to see the result: </p>      
+      <form>
+         <input type = "button" value = "Click Me" onclick = "getValue();" />
+      </form>      
+   </body>
+</html>
